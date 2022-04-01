0ba61e637f5d22dcf169e8ac5a202751999d5675
NekoNeo-README <logicalslot@gmail.com>
2022-04-02 01:03:13 +0300

    -

--- script1.js
+++ script1.js
@@ -1,8 +1,8 @@
-const quotes=["Good software, like wine, takes time - Joel SPolsky", "Another day goes by 🎵 - anon", "Let's go do something today - anon"]
+const quotes=["Good software, like wine, takes time 🍷 - Joel Spolsky", "1 bug fixed, 5 new ones 🪲 - anon", "Pull Request sent ✨ - anon", "Open the source 🔧 - anon", "Emulating CPU operations 🖥️ - anon ", "7 and counting 👩‍💻 - anon", "client-server relationship 🗄️"]
 
 function setup()
 {
-    let qnum = Math.floor(Math.random() * 2) + 0;
+    let qnum = Math.floor(Math.random() * 6) + 0;
     document.getElementById("quote").innerHTML = quotes[qnum];
 
 }
